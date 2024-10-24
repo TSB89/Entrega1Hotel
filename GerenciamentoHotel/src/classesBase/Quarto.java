b6d0008089265f0b7364ac4356189f6c44ef394c
Miguel Maranhao <miguelmaranhao@live.com>
2024-10-24 20:52:13 -0300

    Classe Quarto

--- GerenciamentoHotel/src/classesBase/Quarto.java
+++ GerenciamentoHotel/src/classesBase/Quarto.java
@@ -2,4 +2,36 @@ package Hotel1;
 
 public class Quarto {
 
-}
+	 private int numero;
+	    private String tipo;
+	    private int capacidade;
+	    private double precoDiaria;
+	    private String status;
+
+	    public Quarto(int numero, String tipo, int capacidade, double precoDiaria) {
+	        this.numero = numero;
+	        this.tipo = tipo;
+	        this.capacidade = capacidade;
+	        this.precoDiaria = precoDiaria;
+	        this.status = "disponível";
+	    }
+
+	    public int getNumero() { return numero; }
+	    public String getTipo() { return tipo; }
+	    public int getCapacidade() { return capacidade; }
+	    public double getPrecoDiaria() { return precoDiaria; }
+
+	    @Override
+	    public void atualizarStatus(String status) {
+	        this.status = status;
+	    }
+
+	    public String getStatus() {
+	        return status;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "\nQuarto: " + numero + "\nTipo: " + tipo + " \nCapacidade: " + capacidade + " \nPreço: R$ " + precoDiaria + " \nStatus: " + status + "";
+	    }
+	}
