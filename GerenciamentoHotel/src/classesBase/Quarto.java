551a4d211b7d48acc52f8de4e6617184b4f3f9d5
Miguel Maranhao <miguelmaranhao@live.com>
2024-10-24 21:01:33 -0300

    Classe Quarto

--- GerenciamentoHotel/src/classesBase/Quarto.java
+++ GerenciamentoHotel/src/classesBase/Quarto.java
@@ -1,37 +1,47 @@
-package Hotel1;
+package classesBase;
 
 public class Quarto {
 
-	 private int numero;
-	    private String tipo;
-	    private int capacidade;
-	    private double precoDiaria;
-	    private String status;
+    private int numero;
+    private String tipo;
+    private int capacidade;
+    private double precoDiaria;
+    private boolean disponivel;
 
-	    public Quarto(int numero, String tipo, int capacidade, double precoDiaria) {
-	        this.numero = numero;
-	        this.tipo = tipo;
-	        this.capacidade = capacidade;
-	        this.precoDiaria = precoDiaria;
-	        this.status = "disponível";
-	    }
+    public Quarto(int numero, String tipo, int capacidade, double precoDiaria) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.capacidade = capacidade;
+        this.precoDiaria = precoDiaria;
+        this.disponivel = true;  
+    }
 
-	    public int getNumero() { return numero; }
-	    public String getTipo() { return tipo; }
-	    public int getCapacidade() { return capacidade; }
-	    public double getPrecoDiaria() { return precoDiaria; }
+    public int getNumero() {
+        return numero;
+    }
 
-	    @Override
-	    public void atualizarStatus(String status) {
-	        this.status = status;
-	    }
+    public String getTipo() {
+        return tipo;
+    }
 
-	    public String getStatus() {
-	        return status;
-	    }
+    public int getCapacidade() {
+        return capacidade;
+    }
 
-	    @Override
-	    public String toString() {
-	        return "\nQuarto: " + numero + "\nTipo: " + tipo + " \nCapacidade: " + capacidade + " \nPreço: R$ " + precoDiaria + " \nStatus: " + status + "";
-	    }
-	}
+    public double getPrecoDiaria() {
+        return precoDiaria;
+    }
+
+    public void atualizarStatus(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    @Override
+    public String toString() {
+        return "\nQuarto: " + numero + "\nTipo: " + tipo + " \nCapacidade: " + capacidade + " \nPreço: R$ " + precoDiaria + " \nDisponível: " + (disponivel ? "Sim" : "Não");
+    }
+}
